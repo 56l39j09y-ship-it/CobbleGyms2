67479fdfe25df3b7945b3a9410d75bea10377fb4
56l39j09y-ship-it <56l39j09y@gmail.com>
2026-03-11 14:45:52 -0600

    Add Permissions constants

---
+++ src/main/java/com/cobblegyms/permission/Permissions.java
@@ -0,0 +1,12 @@
+package com.cobblegyms.permission;
+
+public final class Permissions {
+    public static final String ADMIN = "cobblegyms.admin";
+    public static final String GYM_LEADER = "cobblegyms.leader";
+    public static final String GYM_LEADER_OPEN = "cobblegyms.leader.open";
+    public static final String GYM_LEADER_TEAM = "cobblegyms.leader.team";
+    public static final String ELITE_FOUR = "cobblegyms.e4";
+    public static final String CHAMPION = "cobblegyms.champion";
+    public static final String PLAYER = "cobblegyms.player";
+    private Permissions() {}
+}
